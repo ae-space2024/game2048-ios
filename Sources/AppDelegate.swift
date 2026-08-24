e87e83ead5979b33ceb45411007989672d7790cf
ae-space2024 <ae.space@qq.com>
2026-08-24 15:03:56 +0800

    Rewrite: HTML file viewer app

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -6,8 +6,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = ViewController()
+        let vc = ViewController()
+        window?.rootViewController = vc
         window?.makeKeyAndVisible()
+
+        if let url = launchOptions?[.url] as? URL {
+            vc.openFile(url: url)
+        }
         return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        if let vc = window?.rootViewController as? ViewController {
+            vc.openFile(url: url)
+            return true
+        }
+        return false
     }
 }
